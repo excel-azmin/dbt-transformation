cd7a9a33eed8418c8b2f36751463af8038d4f5d5
Shaid Azmin <excel.azmin@gmail.com>
2023-02-01 11:10:08 +0600

    shorted dbt query

--- models/airbyte_tables/serial_no.sql
+++ models/airbyte_tables/serial_no.sql
@@ -12,28 +12,7 @@ select
 from "postgres".public._airbyte_raw_serial_no as table_alias
 -- item
 where 1 = 1
-),  __dbt__cte__serial_no_ab2 as (        
-
-select
-    serial_no,
-    item_code,
-    item_name,
-    warranty
-from __dbt__cte__serial_no_ab1
--- item
-where 1 = 1
-),  __dbt__cte__serial_no_ab3 as (    
-
-select
-    md5(cast(coalesce(cast("serial_no" as text), '') || '-' 
-    || coalesce(cast(item_code as text), '') || '-' 
-    || coalesce(cast(item_name as text), '') || '-'
-    || coalesce(cast(warranty as text), '') as text)) as _airbyte_serial_no_hashid,
-    tmp.*
-from __dbt__cte__serial_no_ab2 tmp
--- item
-where 1 = 1
-)    
+)  
 
 select
     serial_no,
@@ -41,6 +20,6 @@ select
     item_name,
     warranty ->> 'purchaseWarrantyDate' as warranty_date,
     warranty ->> 'purchasedOn' as purchasedOn
-from __dbt__cte__serial_no_ab3
+from __dbt__cte__serial_no_ab1
 -- item from "postgres".public._airbyte_raw_item
 where 1 = 1
